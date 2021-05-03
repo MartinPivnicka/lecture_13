b7271a67ec1305d9fea032c4e19f3c683bce71fc
MartinPivnicka <pivnicka.martin@seznam.cz>
2021-05-03 10:47:17 +0200

    Add fibonacci solution.

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nth_fibo(idx):
+    if idx == 0:
+        return 0
+    elif idx == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(idx - 1) + recursive_nth_fibo(idx - 2)
+
 
 def main():
-    pass
+    idx = int(input("Zadej pocet prvku: "))
+
+    fib_seq = [recursive_nth_fibo(num) for num in range(idx + 1)]
+    print(fib_seq)
 
 
 if __name__ == '__main__':
